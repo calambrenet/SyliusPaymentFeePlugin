798aaa2adf4728ce651ce74ea7c3570472d62a4d
Michal Hošna <mh@michalhosna.com>
2018-08-15 13:35:15 +0200

    CI: Tweak

---
+++ bin/phpstan.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# project root
+cd "$(dirname "$DIR")"
+
+set -x
+vendor/bin/phpstan analyse \
+	--level 6 \
+	--memory-limit 1G \
+	--configuration phpstan.neon \
+	src tests
