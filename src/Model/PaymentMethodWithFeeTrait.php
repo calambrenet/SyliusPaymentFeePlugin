c6296197f3f94fc1a70527818482e814ce734572
Ondřej Kühnel <bibrak.ondra@gmail.com>
2018-08-21 16:18:54 +0200

    add doctrine annotation

--- src/Model/PaymentMethodWithFeeTrait.php
+++ src/Model/PaymentMethodWithFeeTrait.php
@@ -2,15 +2,19 @@
 
 namespace MangoSylius\PaymentFeePlugin\Model;
 
+use Doctrine\ORM\Mapping as ORM;
+
 trait PaymentMethodWithFeeTrait
 {
 	/**
 	 * @var string|null
+	 * @ORM\Column(name="calculator", type="text", nullable=true)
 	 */
 	protected $calculator;
 
 	/**
 	 * @var array
+	 * @ORM\Column(name="calculator_configuration", type="json", nullable=true)
 	 */
 	protected $calculatorConfiguration = [];
 
